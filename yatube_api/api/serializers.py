3f8109bf18482efb68a91968cfa3763b19b18a24
Мария Лисицкая <lisitskayamari@gmail.com>
2021-12-13 12:03:16 +0000

    correct message in FollowSerializers

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -50,7 +50,7 @@ class FollowSerializer(serializers.ModelSerializer):
                 queryset=Follow.objects.all(),
                 fields=('user', 'following'),
                 message=(
-                    'Пользователь и подписавшийся должны быть уникальными')
+                    'Вы не можете подписаться сами на себя')
             )
         ]
 
